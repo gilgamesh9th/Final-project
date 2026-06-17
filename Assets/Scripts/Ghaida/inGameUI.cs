8cf75cb649c38869142e8aa4a0c2a28dcb9648be
Ghaida <ghaida.kh.had@gmail.com>
2026-06-17 23:50:57 +0300

    functional UI
    continue btn and save btn not functional yet and need to add audio mixer for controlling sound n sfx

---
+++ Assets/Scripts/Ghaida/inGameUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class inGameUI : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Debug.Log("ppppew");
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game Scene");
+        Debug.Log("ppppew");
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+    }
+}
