de8822345efcf79d500b75969e32f4be80be72aa
Sultan <280707999+salneif@users.noreply.github.com>
2026-07-04 18:44:57 +0300

    Update PlayerInteraction.cs

--- Assets/Scripts/Turki/PlayerInteraction.cs
+++ Assets/Scripts/Turki/PlayerInteraction.cs
@@ -1,77 +1,77 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class PlayerInteraction : MonoBehaviour
-{
-    private PuzzleItem currentItem;
-    private PuzzleButton currentButton;
-    private RedRecordButton currentRecordButton;
+// public class PlayerInteraction : MonoBehaviour
+// {
+//     private PuzzleItem currentItem;
+//     private PuzzleButton currentButton;
+//     private RedRecordButton currentRecordButton;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (currentRecordButton != null)
-            {
-                currentRecordButton.Interact();
-                return;
-            }
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.E))
+//         {
+//             if (currentRecordButton != null)
+//             {
+//                 currentRecordButton.Interact();
+//                 return;
+//             }
 
-            if (currentButton != null)
-            {
-                currentButton.Interact();
-                return;
-            }
+//             if (currentButton != null)
+//             {
+//                 currentButton.Interact();
+//                 return;
+//             }
 
-            if (currentItem != null)
-            {
-                currentItem.Interact();
-            }
-        }
-    }
+//             if (currentItem != null)
+//             {
+//                 currentItem.Interact();
+//             }
+//         }
+//     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        RedRecordButton recordButton = other.GetComponent<RedRecordButton>();
-        if (recordButton != null)
-        {
-            currentRecordButton = recordButton;
-            return;
-        }
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         RedRecordButton recordButton = other.GetComponent<RedRecordButton>();
+//         if (recordButton != null)
+//         {
+//             currentRecordButton = recordButton;
+//             return;
+//         }
 
-        PuzzleButton button = other.GetComponent<PuzzleButton>();
-        if (button != null)
-        {
-            currentButton = button;
-            return;
-        }
+//         PuzzleButton button = other.GetComponent<PuzzleButton>();
+//         if (button != null)
+//         {
+//             currentButton = button;
+//             return;
+//         }
 
-        PuzzleItem item = other.GetComponent<PuzzleItem>();
-        if (item != null)
-        {
-            currentItem = item;
-        }
-    }
+//         PuzzleItem item = other.GetComponent<PuzzleItem>();
+//         if (item != null)
+//         {
+//             currentItem = item;
+//         }
+//     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        RedRecordButton recordButton = other.GetComponent<RedRecordButton>();
-        if (recordButton != null && recordButton == currentRecordButton)
-        {
-            currentRecordButton = null;
-            return;
-        }
+//     private void OnTriggerExit(Collider other)
+//     {
+//         RedRecordButton recordButton = other.GetComponent<RedRecordButton>();
+//         if (recordButton != null && recordButton == currentRecordButton)
+//         {
+//             currentRecordButton = null;
+//             return;
+//         }
 
-        PuzzleButton button = other.GetComponent<PuzzleButton>();
-        if (button != null && button == currentButton)
-        {
-            currentButton = null;
-            return;
-        }
+//         PuzzleButton button = other.GetComponent<PuzzleButton>();
+//         if (button != null && button == currentButton)
+//         {
+//             currentButton = null;
+//             return;
+//         }
 
-        PuzzleItem item = other.GetComponent<PuzzleItem>();
-        if (item != null && item == currentItem)
-        {
-            currentItem = null;
-        }
-    }
-}
+//         PuzzleItem item = other.GetComponent<PuzzleItem>();
+//         if (item != null && item == currentItem)
+//         {
+//             currentItem = null;
+//         }
+//     }
+// }
