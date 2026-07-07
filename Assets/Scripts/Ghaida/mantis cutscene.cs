0944d2e23d4feffabe2048935dc087c94195c192
Ghaida <ghaida.kh.had@gmail.com>
2026-07-07 19:51:56 +0300

    update

--- Assets/Scripts/Ghaida/mantis cutscene.cs
+++ Assets/Scripts/Ghaida/mantis cutscene.cs
@@ -7,12 +7,17 @@ public class CutsceneTrigger : MonoBehaviour
     public PlayableDirector director;
     public GameObject player;
     public CinemachineCamera cutsceneCam;
+    [SerializeField] private GameObject mantis;
+    [SerializeField] private GameObject bellboy;
 
     public PlayerController playerController;
 
     void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
+
+        mantis.SetActive(true);
+        bellboy.SetActive(false);
 
         playerController.enabled = false;
       
