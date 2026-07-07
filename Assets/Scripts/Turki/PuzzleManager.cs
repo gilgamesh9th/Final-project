a7a5338e87b27fa9a90a8ae18b234dc8ebdb3a8b
vivr44 <turkiare6@gmail.com>
2026-07-07 18:33:19 +0300

    turki3

--- Assets/Scripts/Turki/PuzzleManager.cs
+++ Assets/Scripts/Turki/PuzzleManager.cs
@@ -18,9 +18,6 @@ public class PuzzleManager : MonoBehaviour
     [Header("Door")]
     public Animator doorAnimator;
 
-    [Header("Room Lock")]
-    public GameObject doorBlocker;
-
     [Header("Settings")]
     public float resetDelay = 1.5f;
 
@@ -91,9 +88,6 @@ public class PuzzleManager : MonoBehaviour
 
         if (codeScreen != null)
             codeScreen.SetActive(true);
-
-        if (doorBlocker != null)
-            doorBlocker.SetActive(false);
 
         if (doorAnimator != null)
             doorAnimator.SetTrigger("Open");
